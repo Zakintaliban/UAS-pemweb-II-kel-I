f33aadf13343dc5e9553c95be289ea42bd936548
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-07-19 22:06:24 +0700

    udah jadi sidebar

---
+++ application/views/sidebar.php
@@ -0,0 +1,12 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('program_studi'); ?>">Program Studi</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('kota'); ?>">Kota</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('pejabat'); ?>">Pejabat</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('matakuliah'); ?>">Matakuliah</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('taruna'); ?>">Taruna</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('ijazah'); ?>">Ijazah</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('nilai'); ?>">Nilai</a></li>
+        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('transkrip_nilai'); ?>">Transkrip Nilai</a></li>
+    </ul>
+</nav>
