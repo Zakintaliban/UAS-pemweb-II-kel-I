61edd446de54af5d6a2f19f13409234027fbf25c
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-07-19 22:05:27 +0700

    footer jadi

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,24 +1,35 @@
-  <!-- plugins:js -->
-  <script src="<?php echo base_url('skydash/vendors/js/vendor.bundle.base.js'); ?>"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="<?php echo base_url('skydash/vendors/chart.js/Chart.min.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/vendors/datatables.net/jquery.dataTables.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/vendors/datatables.net-bs4/dataTables.bootstrap4.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/js/dataTables.select.min.js'); ?>"></script>
+<footer class="footer">
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021. Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with<i class="ti-heart text-danger mx-1"></i> by kelompok 1</span>
+  </div>
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span>
+  </div>
+</footer>
+</div>
+</div>
+<!-- plugins:js -->
+<script src="<?php echo base_url('skydash/vendors/js/vendor.bundle.base.js'); ?>"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="<?php echo base_url('skydash/vendors/chart.js/Chart.min.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/vendors/datatables.net/jquery.dataTables.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/vendors/datatables.net-bs4/dataTables.bootstrap4.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/js/dataTables.select.min.js'); ?>"></script>
 
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="<?php echo base_url('skydash/js/off-canvas.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/js/hoverable-collapse.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/js/template.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/js/settings.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/js/todolist.js'); ?>"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="<?php echo base_url('skydash/js/dashboard.js'); ?>"></script>
-  <script src="<?php echo base_url('skydash/js/Chart.roundedBarCharts.js'); ?>"></script>
-  <!-- End custom js for this page-->
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="<?php echo base_url('skydash/js/off-canvas.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/js/hoverable-collapse.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/js/template.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/js/settings.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/js/todolist.js'); ?>"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="<?php echo base_url('skydash/js/dashboard.js'); ?>"></script>
+<script src="<?php echo base_url('skydash/js/Chart.roundedBarCharts.js'); ?>"></script>
+<!-- End custom js for this page-->
 </body>
 
 </html>
