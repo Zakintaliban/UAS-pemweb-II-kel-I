bf481e7237e00b94ec648fe4cfe1321b0b8894fe
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-07-19 22:05:51 +0700

    index jadi

--- application/views/index.php
+++ application/views/index.php
@@ -1,19 +1,7 @@
 <?php $this->load->view('header'); ?>
 
-<h1>Welcome to the University Management Application</h1>
+<?php $this->load->view('sidebar'); ?>
 
-<nav>
-    <ul>
-        <li><a href="<?php echo base_url('program_studi'); ?>">Program Studi</a></li>
-        <li><a href="<?php echo base_url('kota'); ?>">Kota</a></li>
-        <li><a href="<?php echo base_url('pejabat'); ?>">Pejabat</a></li>
-        <li><a href="<?php echo base_url('matakuliah'); ?>">Matakuliah</a></li>
-        <li><a href="<?php echo base_url('taruna'); ?>">Taruna</a></li>
-        <li><a href="<?php echo base_url('ijazah'); ?>">Ijazah</a></li>
-        <li><a href="<?php echo base_url('nilai'); ?>">Nilai</a></li>
-        <li><a href="<?php echo base_url('transkrip_nilai'); ?>">Transkrip Nilai</a></li>
-    </ul>
-</nav>
+<?php $this->load->view('main'); ?>
 
 <?php $this->load->view('footer'); ?>
-
