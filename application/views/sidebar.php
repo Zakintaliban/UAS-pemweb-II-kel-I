acb5fc69a355be9c06060c4a25008b3bb59c6b6e
Fallahi Bagaskara <fallahibagaskara@gmail.com>
2023-07-28 23:52:58 +0700

    add dashboard button

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,5 +1,6 @@
 <nav class="sidebar sidebar-offcanvas active" id="sidebar">
   <ul class="nav">
+    <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('/'); ?>"><i class="mdi mdi-view-dashboard menu-icon"></i><span class="menu-title">Dashboard</span></a></li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('program_studi'); ?>"><i class="mdi mdi-tie menu-icon"></i><span class="menu-title">Program Studi</span></a></li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('kota'); ?>"><i class="mdi mdi-map-marker menu-icon"></i><span class="menu-title">Kota</span></a></li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('pejabat'); ?>"><i class="mdi mdi-account-multiple menu-icon"></i><span class="menu-title">Pejabat</span></a></li>
